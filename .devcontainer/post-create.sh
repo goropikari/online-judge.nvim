458d5ea0a6b7cbc2454e5f08c916d70d09f63d05
goropikari <goropikari56@gmail.com>
2024-12-17 23:14:31 +0900

    change venv dir

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-python3 -m venv venv
-venv/bin/pip install git+https://github.com/online-judge-tools/oj@v12.0.0
-venv/bin/pip install debugpy==1.8.11
-echo 'source /workspaces/atcoder.nvim/venv/bin/activate' >> ~/.bashrc
+python3 -m venv ~/venv
+~/venv/bin/pip install git+https://github.com/online-judge-tools/oj@v12.0.0
+~/venv/bin/pip install debugpy==1.8.11
+echo 'source ~/venv/bin/activate' >> ~/.bashrc
