78d82fbcb0ac31ab82d36327bad4b79d738fa545
goropikari <goropikari56@gmail.com>
2024-12-18 22:16:39 +0900

    update devcontainer neovim plugin

--- .devcontainer/nvim/init.lua
+++ .devcontainer/nvim/init.lua
@@ -130,7 +130,33 @@ require('lazy').setup({
       },
       opts = {},
     },
-    -- { import = 'plugins' },
+    {
+      'nvim-treesitter/nvim-treesitter',
+      dependencies = {
+        'nvim-treesitter/nvim-treesitter-textobjects',
+      },
+      build = ':TSUpdate',
+      config = function()
+        vim.defer_fn(function()
+          require('nvim-treesitter.configs').setup({
+            ensure_installed = { 'cpp', 'python' },
+          })
+        end, 0)
+      end,
+    },
+    {
+      'williamboman/mason.nvim',
+      opts = {},
+    },
+    {
+      'WhoIsSethDaniel/mason-tool-installer.nvim',
+      dependencies = {
+        'williamboman/mason.nvim',
+      },
+      opts = {
+        ensure_installed = { 'codelldb', 'cpptools' },
+      },
+    },
   },
   install = { colorscheme = { 'habamax' } },
   checker = { enabled = true },
